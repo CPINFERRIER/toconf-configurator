47cdf1611105395fbf9f2bd2dbe42c9dadd285da
Cyril PIN-FERRIER <cyril.pinferrier@outlook.fr>
2023-02-05 14:05:31 +0100

    Ajoutez des fichiers projet.

---
+++ toconf configurator/Form1.Designer.cs
@@ -0,0 +1,441 @@
+﻿namespace toconf_configurator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.ciblename = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.adh = new System.Windows.Forms.TextBox();
+            this.adm = new System.Windows.Forms.TextBox();
+            this.ads = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.decpm = new System.Windows.Forms.TextBox();
+            this.dech = new System.Windows.Forms.TextBox();
+            this.decm = new System.Windows.Forms.TextBox();
+            this.decs = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.year = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.mount = new System.Windows.Forms.TextBox();
+            this.day = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.obscde = new System.Windows.Forms.TextBox();
+            this.ext = new System.Windows.Forms.TextBox();
+            this.gen = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.nametxt = new System.Windows.Forms.TextBox();
+            this.empl = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.nametxt1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom cible";
+            // 
+            // ciblename
+            // 
+            this.ciblename.Location = new System.Drawing.Point(160, 16);
+            this.ciblename.Name = "ciblename";
+            this.ciblename.Size = new System.Drawing.Size(63, 20);
+            this.ciblename.TabIndex = 1;
+            this.ciblename.Text = "CH00001";
+            this.ciblename.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "AD";
+            // 
+            // adh
+            // 
+            this.adh.Location = new System.Drawing.Point(198, 120);
+            this.adh.Name = "adh";
+            this.adh.Size = new System.Drawing.Size(25, 20);
+            this.adh.TabIndex = 3;
+            this.adh.Text = "00";
+            this.adh.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // adm
+            // 
+            this.adm.Location = new System.Drawing.Point(229, 120);
+            this.adm.Name = "adm";
+            this.adm.Size = new System.Drawing.Size(25, 20);
+            this.adm.TabIndex = 4;
+            this.adm.Text = "00";
+            this.adm.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ads
+            // 
+            this.ads.Location = new System.Drawing.Point(260, 120);
+            this.ads.Name = "ads";
+            this.ads.Size = new System.Drawing.Size(25, 20);
+            this.ads.TabIndex = 5;
+            this.ads.Text = "00";
+            this.ads.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "DEC";
+            // 
+            // decpm
+            // 
+            this.decpm.Location = new System.Drawing.Point(167, 146);
+            this.decpm.Name = "decpm";
+            this.decpm.Size = new System.Drawing.Size(25, 20);
+            this.decpm.TabIndex = 7;
+            this.decpm.Text = "+";
+            this.decpm.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dech
+            // 
+            this.dech.Location = new System.Drawing.Point(198, 146);
+            this.dech.Name = "dech";
+            this.dech.Size = new System.Drawing.Size(25, 20);
+            this.dech.TabIndex = 8;
+            this.dech.Text = "00";
+            this.dech.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // decm
+            // 
+            this.decm.Location = new System.Drawing.Point(229, 146);
+            this.decm.Name = "decm";
+            this.decm.Size = new System.Drawing.Size(25, 20);
+            this.decm.TabIndex = 9;
+            this.decm.Text = "00";
+            this.decm.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // decs
+            // 
+            this.decs.Location = new System.Drawing.Point(260, 146);
+            this.decs.Name = "decs";
+            this.decs.Size = new System.Drawing.Size(25, 20);
+            this.decs.TabIndex = 10;
+            this.decs.Text = "00";
+            this.decs.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Année";
+            // 
+            // year
+            // 
+            this.year.Location = new System.Drawing.Point(160, 42);
+            this.year.Name = "year";
+            this.year.Size = new System.Drawing.Size(63, 20);
+            this.year.TabIndex = 12;
+            this.year.Text = "2023";
+            this.year.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(229, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "(maximum 7 caractéres)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(76, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Mois";
+            // 
+            // mount
+            // 
+            this.mount.Location = new System.Drawing.Point(160, 68);
+            this.mount.Name = "mount";
+            this.mount.Size = new System.Drawing.Size(63, 20);
+            this.mount.TabIndex = 15;
+            this.mount.Text = "01";
+            this.mount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // day
+            // 
+            this.day.Location = new System.Drawing.Point(160, 94);
+            this.day.Name = "day";
+            this.day.Size = new System.Drawing.Size(63, 20);
+            this.day.TabIndex = 16;
+            this.day.Text = "01";
+            this.day.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(76, 97);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Jour";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(76, 175);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Obs Code";
+            // 
+            // obscde
+            // 
+            this.obscde.Location = new System.Drawing.Point(160, 172);
+            this.obscde.Name = "obscde";
+            this.obscde.Size = new System.Drawing.Size(63, 20);
+            this.obscde.TabIndex = 19;
+            this.obscde.Text = "M10";
+            this.obscde.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ext
+            // 
+            this.ext.Location = new System.Drawing.Point(13, 198);
+            this.ext.Name = "ext";
+            this.ext.ReadOnly = true;
+            this.ext.Size = new System.Drawing.Size(410, 20);
+            this.ext.TabIndex = 20;
+            // 
+            // gen
+            // 
+            this.gen.Location = new System.Drawing.Point(69, 314);
+            this.gen.Name = "gen";
+            this.gen.Size = new System.Drawing.Size(94, 23);
+            this.gen.TabIndex = 21;
+            this.gen.Text = "Générer ligne";
+            this.gen.UseVisualStyleBackColor = true;
+            this.gen.Click += new System.EventHandler(this.gen_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(269, 314);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(94, 23);
+            this.exit.TabIndex = 22;
+            this.exit.Text = "Quitter";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(169, 314);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 23);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "Générer fichier";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(70, 277);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Nom fichier";
+            // 
+            // nametxt
+            // 
+            this.nametxt.Location = new System.Drawing.Point(154, 274);
+            this.nametxt.Name = "nametxt";
+            this.nametxt.Size = new System.Drawing.Size(63, 20);
+            this.nametxt.TabIndex = 26;
+            this.nametxt.Text = "toconf";
+            this.nametxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // empl
+            // 
+            this.empl.Location = new System.Drawing.Point(198, 242);
+            this.empl.Name = "empl";
+            this.empl.Size = new System.Drawing.Size(174, 20);
+            this.empl.TabIndex = 27;
+            this.empl.Text = "C:\\Users\\PIN-FERRIER\\Desktop\\";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(42, 245);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(151, 13);
+            this.label10.TabIndex = 28;
+            this.label10.Text = "Emplacement d\'enregistrement";
+            // 
+            // nametxt1
+            // 
+            this.nametxt1.Location = new System.Drawing.Point(223, 274);
+            this.nametxt1.Name = "nametxt1";
+            this.nametxt1.Size = new System.Drawing.Size(31, 20);
+            this.nametxt1.TabIndex = 29;
+            this.nametxt1.Text = "10";
+            this.nametxt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(260, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 21);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(322, 277);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(13, 13);
+            this.label11.TabIndex = 31;
+            this.label11.Text = "1";
+            this.label11.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(291, 274);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(25, 21);
+            this.button3.TabIndex = 32;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 349);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nametxt1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.empl);
+            this.Controls.Add(this.nametxt);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.gen);
+            this.Controls.Add(this.ext);
+            this.Controls.Add(this.obscde);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.day);
+            this.Controls.Add(this.mount);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.year);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.decs);
+            this.Controls.Add(this.decm);
+            this.Controls.Add(this.dech);
+            this.Controls.Add(this.decpm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ads);
+            this.Controls.Add(this.adm);
+            this.Controls.Add(this.adh);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ciblename);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "toconf Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ciblename;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox adh;
+        private System.Windows.Forms.TextBox adm;
+        private System.Windows.Forms.TextBox ads;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox decpm;
+        private System.Windows.Forms.TextBox dech;
+        private System.Windows.Forms.TextBox decm;
+        private System.Windows.Forms.TextBox decs;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox year;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox mount;
+        private System.Windows.Forms.TextBox day;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox obscde;
+        private System.Windows.Forms.TextBox ext;
+        private System.Windows.Forms.Button gen;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox nametxt;
+        private System.Windows.Forms.TextBox empl;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox nametxt1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ toconf configurator/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace toconf_configurator
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gen_Click(object sender, EventArgs e)
+        {
+            ext.Text = "     " + ciblename.Text + " KC" + year.Text + " " + day.Text + " " + mount.Text + ".00000" + " " + 
+                adh.Text + " " + adm.Text + " " + ads.Text +".00" + " " + decpm.Text + dech.Text + " " + decm.Text + " " +
+                decs.Text +".0" + "                      " + obscde.Text;
+            Clipboard.SetText(ext.Text);
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+           Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fileName = nametxt.Text + nametxt1.Text + ".txt";
+            File.WriteAllText(empl.Text + fileName, "     " + ciblename.Text + " KC" + year.Text + " " + day.Text + " " + mount.Text + ".00000" + " " +
+                adh.Text + " " + adm.Text + " " + ads.Text + ".00" + " " + decpm.Text + dech.Text + " " + decm.Text + " " +
+                decs.Text + ".0" + "                      " + obscde.Text);
+
+            var A = int.Parse(nametxt1.Text);
+            var B = int.Parse(label11.Text);
+            var C = A + B;
+            nametxt1.Text = C.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var A = int.Parse(nametxt1.Text);
+            var B = int.Parse(label11.Text);
+            var C = A + B;
+            nametxt1.Text = C.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var A = int.Parse(nametxt1.Text);
+            var B = int.Parse(label11.Text);
+            var C = A - B;
+            nametxt1.Text = C.ToString();
+        }
+    }
+}
